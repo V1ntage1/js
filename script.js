21fd320916a5b32be234567d1d8dd00a8f96edb4
V1ntage1 <90610502+V1ntage1@users.noreply.github.com>
2021-11-10 11:00:47 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var a = prompt("введите чилсо")
+a = parseInt(a);
+if (a>20){
+    alert("a боль")
+}
+else
+{
+    alert("Вы нам не подходите");
+}
+
+
---
+++ themes.js
@@ -0,0 +1,12 @@
+var a = prompt("сколько вам лет?");
+a = parseInt(a);
+if (a>20)
+{
+    alert(a>20)
+    document.write("Вы нам подходите.")
+}
+else{
+    alert(a<20);
+    document.write("Вы нам не подходите.")
+}
+
